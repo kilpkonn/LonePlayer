a55e0b1305281648b5a547ebeefaa35b2b667c0e
taannu <tavo.annus@gmail.com>
2019-04-16 17:54:22 +0300

    Repeat fix

--- core/src/ee/taltech/iti0202/gui/game/desktop/entities/animated/MyPlayerTweener.java
+++ core/src/ee/taltech/iti0202/gui/game/desktop/entities/animated/MyPlayerTweener.java
@@ -1,16 +1,31 @@
 package ee.taltech.iti0202.gui.game.desktop.entities.animated;
 
+import com.brashmonkey.spriter.Animation;
 import com.brashmonkey.spriter.Entity;
 import com.brashmonkey.spriter.PlayerTweener;
+
+import java.util.HashSet;
 
 import ee.taltech.iti0202.gui.game.desktop.handlers.variables.B2DVars;
 
 public class MyPlayerTweener extends PlayerTweener {
     private float influence;
     private boolean playingOnce;
+    private Animation prevAnimation;
+    private HashSet<String> animToPlayOnce = new HashSet<>();
 
     public MyPlayerTweener(Entity entity) {
         super(entity);
+        getSecondPlayer().addListener(new MyPlayerListener(new Runnable() {
+            @Override
+            public void run() {
+                if (playingOnce) {
+                    setAnimation(prevAnimation.name, false); //TODO: Safer launch -> handle return on first anim
+                    influence = 1;
+                    playingOnce = false;
+                }
+            }
+        }));
     }
 
     public void update(float dt) {
@@ -22,31 +37,30 @@ public class MyPlayerTweener extends PlayerTweener {
         super.update(dt);
     }
 
-    public void setAnimation(String anim) {
+    /*public void setAnimation(String anim) {
         if (!anim.equals(getSecondPlayer().getAnimation().name)) {
             influence = 0;
             getFirstPlayer().setAnimation(getSecondPlayer().getAnimation());
             getSecondPlayer().setAnimation(anim);
         }
-    }
+    }*/
 
-    public void setAnimation(String anim, final boolean playOnce) {
+    public void setAnimation(String anim, boolean playOnce) {
         if (!anim.equals(getSecondPlayer().getAnimation().name)) {
-            influence = anim.equals("roll") ? 0.5f : 0; //TODO: Detect roll end and return
+            if (animToPlayOnce.contains(anim)) {
+                influence = 0.5f;
+            } else if (animToPlayOnce.contains(getSecondPlayer().getAnimation().name)) {
+                influence = 1;
+            } else {
+                influence = 0;
+            }
             getFirstPlayer().setAnimation(getSecondPlayer().getAnimation());
             getSecondPlayer().setAnimation(anim);
-            if (playOnce) {
-                getSecondPlayer().addListener(new MyPlayerListener(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (playingOnce) {
-                            setAnimation(getFirstPlayer().getAnimation().name, false);
-                            influence = 1;
-                            playingOnce = false;
-                        }
-                    }
-                }));
-                playingOnce = true;
+
+            playingOnce = playOnce;
+
+            if (!animToPlayOnce.contains(getFirstPlayer().getAnimation().name)) {
+                prevAnimation = getFirstPlayer().getAnimation();
             }
         }
     }
@@ -54,5 +68,9 @@ public class MyPlayerTweener extends PlayerTweener {
     @Override
     public void setEntity(Entity entity) {
         super.setEntity(entity);
+    }
+
+    public void setAnimToPlayOnce(HashSet<String> animations) {
+        this.animToPlayOnce = animations;
     }
 }
