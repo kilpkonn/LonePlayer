d83b4d2bcaa6d2ea83a6b968598294e1bf89822b
taannu <tavo.annus@gmail.com>
2019-05-07 09:47:42 +0300

    comment out test code

--- core/src/ee/taltech/iti0202/gui/game/desktop/entities/projectile/bullet/handler/BulletHandler.java
+++ core/src/ee/taltech/iti0202/gui/game/desktop/entities/projectile/bullet/handler/BulletHandler.java
@@ -18,7 +18,7 @@ public class BulletHandler implements Handler {
     public void update(float dt) {
         Array<Bullet> toBeRemoved = new Array<>();
         for (Bullet bullet : bulletArray) {
-            if (bullet.getBody().getLinearVelocity().x < 5) bullet.onHit();
+            //if (bullet.getBody().getLinearVelocity().x < 5) bullet.onHit();
 
             if (bullet.toBeRemoved()) {
                 toBeRemoved.add(bullet);
